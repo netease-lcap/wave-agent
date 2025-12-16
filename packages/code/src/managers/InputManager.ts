47510ecb9c7a4357d5b53d348e4087328f49d8ef
lewis617 <lewis617@163.com>
2025-12-16 17:19:22 +0800

    fix: remove searchFiles

--- packages/code/src/managers/InputManager.ts
+++ packages/code/src/managers/InputManager.ts
@@ -1,5 +1,5 @@
 import { FileItem } from "../components/FileSelector.js";
-import { searchFiles as searchFilesUtil } from "../utils/fileSearch.js";
+import { searchFilesRipgrep as searchFilesUtil } from "wave-agent-sdk";
 import { readClipboardImage } from "../utils/clipboard.js";
 import type { Key } from "ink";
 
